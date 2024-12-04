27c504a0aedecc15e3f31391d1354b4b766dda09
Khaled CHEHAB <lynckit@hotmail.com>
2024-12-04 08:35:06 +0100

    Réorganisation de l'ordre des middlewares et ajout de CORS dans index.js

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const jokeRouter = require('./routes/jokes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./init/swaggerConfig');
 const initializeDatabase = require('./init/dbInit');
-console.log(typeof initializeDatabase);
+const cors = require('cors');
 
 const app = express();
-app.use(express.json());
 
-const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
